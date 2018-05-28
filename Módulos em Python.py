946dd0f9bfaa084cda94fdf0faec4c76b9d652a1
Felipe Oliveira Bueno de Camargo <felipe.c@outlook.com>
2018-05-28 17:18:07 -0300

    Add files via upload

---
+++ Módulos em Python.py
@@ -0,0 +1,8 @@
+import math
+
+math.ceil() # arredonda pra cima 
+math.floor()# arrendonda pra baixo
+math.trunc() # apenes numero antes de virgula
+math.pow() # calcula a potencia
+math.sqrt() # calcula a raiz quadrada
+math.factorial() # calcular o fatorial
