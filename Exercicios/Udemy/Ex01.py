94b20d484f723790619720d2cd267fb3f2d10653
Felipe Oliveira Bueno de Camargo <felipe.c@outlook.com>
2019-05-22 14:09:46 -0300

    Ex's

---
+++ Exercicios/Udemy/Ex01.py
@@ -0,0 +1,5 @@
+Nome = 'Felipe'
+Primeiro_Sobrenome = 'Oliveira'
+Ultimo_Nome = 'Bueno'
+
+print(Nome[0], Primeiro_Sobrenome[0], Ultimo_Nome[0])
---
+++ Exercicios/Udemy/Ex02.py
@@ -0,0 +1,6 @@
+Nome = 'felipe'
+empresa = 'cluster2Go'
+Ultimo_nome = 'bueno'
+
+print(Nome+'.'+Ultimo_nome+'@'+empresa+'.com')
+
